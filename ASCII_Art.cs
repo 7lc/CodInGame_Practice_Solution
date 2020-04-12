368f0f9b315c55276bbcdb43cb044da1ce0086e8
Longchang Cui <cuilongchang@hotmail.com>
2020-04-11 20:19:03 -0400

    Create ASCII_Art.cs

---
+++ ASCII_Art.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int L = int.Parse(Console.ReadLine());
+        int H = int.Parse(Console.ReadLine());
+        string T = Console.ReadLine();
+        
+        Dictionary<char,int> dict = new Dictionary<char,int>()
+        {
+            {'a',1},
+            {'b',2},
+            {'c',3},
+            {'d',4},
+            {'e',5},
+            
+            {'f',6},
+            {'g',7},
+            {'h',8},
+            {'i',9},
+            {'j',10},
+            
+            {'k',11},
+            {'l',12},
+            {'m',13},
+            {'n',14},
+            {'o',15},
+            
+            {'p',16},
+            {'q',17},
+            {'r',18},
+            {'s',19},
+            {'t',20},
+            
+            {'u',21},
+            {'v',22},
+            {'w',23},
+            {'x',24},
+            {'y',25},
+            {'z',26},
+        };
+        
+        char[] char_arr = T.ToLower().ToCharArray();
+        int idx = 0;
+        
+        for (int i = 0; i < H; i++)
+        {
+            string ROW = Console.ReadLine();
+            char[] char_row = ROW.ToCharArray();
+            
+            // Print each character line
+            for(int j = 0; j < char_arr.Length;j++)
+            {
+                // if character is in dictionary ([a-z],[A-Z])
+                if(dict.ContainsKey(char_arr[j]) == true)
+                    idx = dict[char_arr[j]] * L;
+                
+                // else print the question mark
+                else
+                    idx = 27 * L;
+                    
+                for(int k = L; k >= 1; k--)
+                {
+                   Console.Write(char_row[idx-k]);
+                }
+            }
+            
+            Console.Write("\n");
+        }
+
+    }
+}
